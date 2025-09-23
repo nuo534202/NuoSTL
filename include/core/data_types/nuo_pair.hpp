1473733e0ed033260710e0857fec09a18ed71601
nuo534202 <t330026229@mail.uic.edu.cn>
2025-09-23 23:18:44 +0800

    add declaratrion of get methods in nuo_pair.hpp

--- include/core/data_types/nuo_pair.hpp
+++ include/core/data_types/nuo_pair.hpp
@@ -2,6 +2,7 @@
 #define NUOSTL_CORE_DATA_TYPES_NUO_PAIR_HPP_
 
 #include <stddef.h>
+
 #include <type_traits>
 #include <utility>
 
@@ -9,6 +10,8 @@ namespace nuostl {
 
 template<typename T1, typename T2>
 class nuo_pair {
+private:
+    void nuo_type_check(const nuo_pair& np) const;
 public:
     T1 first;
     T2 second;
@@ -26,9 +29,11 @@ public:
     constexpr nuo_pair(nuo_pair&&) = default;
 
     /* Operator */
+    /* Group 0 */
     nuo_pair& operator=(const nuo_pair&) = default;
     nuo_pair& operator=(nuo_pair&&) = default;
 
+    /* Group 1 */
     bool operator<(const nuo_pair& np) const;
     bool operator<=(const nuo_pair& np) const;
     bool operator>(const nuo_pair& np) const;
@@ -36,11 +41,13 @@ public:
     bool operator==(const nuo_pair& np) const;
     bool operator!=(const nuo_pair& np) const;
 
+    /* Group 2 */
     nuo_pair operator+(const nuo_pair& np) const;
     nuo_pair operator-(const nuo_pair& np) const;
     nuo_pair operator*(const nuo_pair& np) const;
     nuo_pair operator/(const nuo_pair& np) const;
 
+    /* Group 3 */
     nuo_pair& operator+=(const nuo_pair& np);
     nuo_pair& operator-=(const nuo_pair& np);
     nuo_pair& operator*=(const nuo_pair& np);
@@ -59,11 +66,33 @@ nuo_pair<T1, T2> nuo_make_pair(T1 _first, T2 _second) {
     return nuo_pair<T1, T2>(_first, _second);
 }
 
-/* Todo: get */
-
-} /* namespace */
+} /* namespace nuostl */
 
 namespace std {
+
+/* for left value */
+template<size_t N, typename T1, typename T2>
+constexpr auto& get(nuostl::nuo_pair<T1, T2>& p) noexcept {
+    static_assert(N < 2, "Index out of range in nuo_pair::get");
+    if constexpr (N == 0) return p.first;
+    else return p.second;
+}
+
+/* for const left value */
+template<size_t N, typename T1, typename T2>
+constexpr const auto& get(const nuostl::nuo_pair<T1, T2>& p) noexcept {
+    static_assert(N < 2, "Index out of range in nuo_pair::get");
+    if constexpr (N == 0) return p.first;
+    else return p.second;
+}
+
+/* for right value */
+template<size_t N, typename T1, typename T2>
+constexpr auto&& get(nuostl::nuo_pair<T1, T2>& p) noexcept {
+    static_assert(N < 2, "Index out of range in nuo_pair::get");
+    if constexpr (N == 0) return move(p.first);
+    else return move(p.second);
+}
 
 template<typename T1, typename T2>
 struct tuple_size<nuostl::nuo_pair<T1, T2>> :
@@ -75,6 +104,6 @@ struct tuple_element<N, nuostl::nuo_pair<T1, T2>> {
     using type = std::conditional_t<N == 0, T1, T2>;
 };
 
-} /* namespace */
+} /* namespace std */
 
 #endif
