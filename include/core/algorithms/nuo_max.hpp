dae43626b17923ece875d7c4e961b95aa8f438c4
nuo534202 <t330026229@mail.uic.edu.cn>
2025-09-30 17:08:49 +0800

    create nuo_max and nuo_min

--- include/core/algorithms/nuo_max.hpp
+++ include/core/algorithms/nuo_max.hpp
@@ -2,29 +2,69 @@
 #define NUOSTL_CORE_ALGORITHMS_NUO_MAX_HPP_
 
 #include <concepts>
+#include <initializer_list>
+#include <iterator>
 
 namespace nuostl {
 
 template<typename T>
 constexpr const T& nuo_max(const T& a, const T& b) {
-    if constexpr (requires (const T& x, const T& y) {
-        { x < y } -> std::convertible_to<bool>;
-    })
-    {
+    if constexpr (requires(const T& x, const T& y) {
+                                    { x < y } -> std::convertible_to<bool>;
+                                }) {
         return (a < b) ? b : a;
-    } else if constexpr (requires (const T& x, const T& y) {
-        { x > y } -> std::convertible_to<bool>;
-    })
+    } else if constexpr (requires(const T& x, const T& y) {
+            { x > y } -> std::convertible_to<bool>;
+        })
     {
         return (a > b) ? a : b;
-    } else
-    {
+    } else {
         /* unreachable, for well-formedness */
-        static_assert(!sizeof(T), "nuo_max: T must define operator< or operator>");
+        static_assert(!sizeof(T),
+            "nuo_max: T must define operator< or operator>");
         return a;
     }
 }
 
-}   /* namespace nuostl */
+template<typename T, typename... Ts>
+constexpr const T nuo_max(const T& a, const T& b, const Ts&... rest) {
+    const T& ans = nuo_max(a, b);
+    if constexpr (sizeof...(rest) == 0) {
+        return ans;
+    } else {
+        return nuo_max(ans, rest...);
+    }
+}
+
+template<typename T>
+constexpr T nuo_max(std::initializer_list<T> ilist) {
+    if (ilist.size() == 0) {
+        return T{};
+    }
+    auto it = ilist.begin();
+    T best = *it;
+    it++;
+    for (; it != ilist.end(); it++) {
+        best = nuo_max(best, *it);
+    }
+    return best;
+}
+
+template<typename Iter>
+constexpr auto nuo_max(Iter first, Iter last)
+        -> typename std::iterator_traits<Iter>::value_type {
+    using T = typename std::iterator_traits<Iter>::value_type;
+    if (first == last) {
+        return T{};
+    }
+    T best = *first;
+    first++;
+    for (; first != last; ++first) {
+        best = nuo_max(best, *first);
+    }
+    return best;
+}
+
+} /* namespace nuostl */
 
 #endif
--- include/core/algorithms/nuo_min.hpp
+++ include/core/algorithms/nuo_min.hpp
@@ -2,6 +2,8 @@
 #define NUOSTL_CORE_ALGORITHMS_NUO_MIN_HPP_
 
 #include <concepts>
+#include <initializer_list>
+#include <iterator>
 
 namespace nuostl {
 
@@ -23,6 +25,45 @@ constexpr const T& nuo_min(const T& a, const T& b) {
         static_assert(!sizeof(T), "nuo_min: T must define operator< or operator>");
         return a;
     }
+}
+
+template<typename T, typename... Ts>
+constexpr const T nuo_min(const T& a, const T& b, const Ts&... rest) {
+    const T& ans = nuo_min(a, b);
+    if (sizeof...(rest) == 0) {
+        return ans;
+    } else {
+        return nuo_min(ans, rest...);
+    }
+}
+
+template<typename T>
+constexpr T nuo_min(std::initializer_list<T> ilist) {
+    if (ilist.size() == 0)
+        return T{};
+    
+    auto it = ilist.begin();
+    T ans = *it;
+    it++;
+    for (; it != ilist.end(); it++)
+        ans = nuo_min(ans, *it);
+
+    return ans;
+}
+
+template<typename Iter>
+constexpr auto nuo_min(Iter first, Iter last)
+        -> typename std::iterator_traits<Iter>::value_type {
+    using T = typename std::iterator_traits<Iter>::value_type;
+    if (first == last)
+        return T{};
+
+    T ans = *first;
+    first++;
+    for (; first != last; first++)
+        ans = nuo_min(ans, *first);
+
+    return ans;
 }
 
 }   /* namespace nuostl*/
