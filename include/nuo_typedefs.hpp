0d6298dc758a11cac728ba3a34c9e080965e0bd2
nuo <nuo534202@nuodeMacBook-Air.local>
2025-10-21 21:32:16 +0800

    modify idx_t

--- include/nuo_typedefs.hpp
+++ include/nuo_typedefs.hpp
@@ -1,11 +1,12 @@
 #ifndef NUOSTL_NUO_TYPEDEFS_HPP_
 #define NUOSTL_NUO_TYPEDEFS_HPP_
 
+#include <stddef.h>
 #include <stdint.h>
 
 namespace nuostl {
 
-typedef uint64_t idx_t;
+using idx_t = size_t;
 
 }   /* namespace nuostl */
 
